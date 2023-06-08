2324387ad9c63db7c3f8903445afa259f45f8f30
MiYanDoris <dorisyan@pku.edu.cn>
2023-06-08 16:57:47 +0800

    empty initialization instead of zero fill

--- torchprimitivesdf/sdf.py
+++ torchprimitivesdf/sdf.py
@@ -29,25 +29,30 @@ def box_sdf(points, box):
 class _BoxDistanceCuda(torch.autograd.Function):
     @staticmethod
     def forward(ctx, points, box):
+        if not points.is_contiguous():
+            points = points.contiguous()
+        if not box.is_contiguous():
+            box = box.contiguous()
         num_points = points.shape[0]
-        distances = torch.zeros([num_points], device=points.device, dtype=points.dtype)
-        dis_signs = torch.zeros([num_points], device=points.device, dtype=torch.bool)
-        closest_points = torch.zeros([num_points, 3], device=points.device, dtype=points.dtype)
+        distances = torch.empty([num_points], device=points.device, dtype=points.dtype)
+        dis_signs = torch.empty([num_points], device=points.device, dtype=torch.bool)
+        closest_points = torch.empty([num_points, 3], device=points.device, dtype=points.dtype)
         if points.is_cuda:
             _C.box_distance_forward_cuda(points, box, distances, dis_signs, closest_points)
         else:
             _C.box_distance_forward(points, box, distances, dis_signs, closest_points)
         # _C.box_distance_forward_cuda(points, box, distances, dis_signs, closest_points)
         # _C.box_distance_forward(points, box, distances, dis_signs, closest_points)
-        ctx.save_for_backward(points.contiguous(), closest_points)
+        ctx.save_for_backward(points, closest_points)
         ctx.mark_non_differentiable(dis_signs, closest_points)
         return distances, dis_signs, closest_points
     
     @staticmethod
     def backward(ctx, grad_distances, grad_dis_signs, grad_closest_points):
         points, closest_points = ctx.saved_tensors
-        grad_distances = grad_distances.contiguous()
-        grad_points = torch.zeros_like(points)
+        if not grad_distances.is_contiguous():
+            grad_distances = grad_distances.contiguous()
+        grad_points = torch.empty_like(points)
         grad_box = None
         # if points.is_cuda:
         #     _C.box_distance_backward_cuda(grad_distances, points, closest_points, grad_points)
@@ -72,10 +77,10 @@ class _TransformPointsInverse(torch.autograd.Function):
                 translations = translations.contiguous()
             if not rotations.is_contiguous():
                 rotations = rotations.contiguous()
-            points_transformed = torch.zeros_like(points)
+            points_transformed = torch.empty_like(points)
             _C.transform_points_inverse_forward_cuda(points, translations, rotations, points_transformed)
         else:
-            points_transformed = torch.zeros_like(points)
+            points_transformed = torch.empty_like(points)
             _C.transform_points_inverse_forward(points, translations, rotations, points_transformed)
         ctx.save_for_backward(points, translations, rotations)
         return points_transformed
@@ -86,14 +91,14 @@ class _TransformPointsInverse(torch.autograd.Function):
         if points.is_cuda:
             if not grad_points_transformed.is_contiguous():
                 grad_points_transformed = grad_points_transformed.contiguous()
-            grad_points = torch.zeros_like(points)
-            grad_translations = torch.zeros_like(translations)
-            grad_rotations = torch.zeros_like(rotations)
+            grad_points = torch.empty_like(points)
+            grad_translations = torch.empty_like(translations)
+            grad_rotations = torch.empty_like(rotations)
             _C.transform_points_inverse_backward_cuda(grad_points_transformed, points, translations, rotations, grad_points, grad_translations, grad_rotations)
         else:
-            grad_points = torch.zeros_like(points)
-            grad_translations = torch.zeros_like(translations)
-            grad_rotations = torch.zeros_like(rotations)
+            grad_points = torch.empty_like(points)
+            grad_translations = torch.empty_like(translations)
+            grad_rotations = torch.empty_like(rotations)
             _C.transform_points_inverse_backward(grad_points_transformed, points, translations, rotations, grad_points, grad_translations, grad_rotations)
         return grad_points, grad_translations, grad_rotations
 
@@ -112,10 +117,10 @@ class _FixedTransformPointsInverse(torch.autograd.Function):
                 translations = translations.contiguous()
             if not rotations.is_contiguous():
                 rotations = rotations.contiguous()
-            points_transformed = torch.zeros_like(points)
+            points_transformed = torch.empty_like(points)
             _C.fixed_transform_points_inverse_forward_cuda(points, translations, rotations, points_transformed)
         else:
-            points_transformed = torch.zeros_like(points)
+            points_transformed = torch.empty_like(points)
             _C.fixed_transform_points_inverse_forward(points, translations, rotations, points_transformed)
         ctx.save_for_backward(points, translations, rotations)
         return points_transformed
@@ -126,9 +131,9 @@ class _FixedTransformPointsInverse(torch.autograd.Function):
         if points.is_cuda:
             if not grad_points_transformed.is_contiguous():
                 grad_points_transformed = grad_points_transformed.contiguous()
-            grad_points = torch.zeros_like(points)
+            grad_points = torch.empty_like(points)
             _C.fixed_transform_points_inverse_backward_cuda(grad_points_transformed, points, translations, rotations, grad_points)
         else:
-            grad_points = torch.zeros_like(points)
+            grad_points = torch.empty_like(points)
             _C.fixed_transform_points_inverse_backward(grad_points_transformed, points, translations, rotations, grad_points)
         return grad_points, None, None
